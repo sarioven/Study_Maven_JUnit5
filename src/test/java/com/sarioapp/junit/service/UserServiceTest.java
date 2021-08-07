b3d1a0b1a5767e8a5aa2647422d58b44c8cacc97
so_happy <sarioven1@gmail.com>
2021-08-08 00:21:30 +0300

    Project prepared

---
+++ src/test/java/com/sarioapp/junit/service/UserServiceTest.java
@@ -0,0 +1,13 @@
+package com.sarioapp.junit.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class UserServiceTest {
+
+    @Test
+    void test() {
+        assertTrue(false);
+    }
+}
