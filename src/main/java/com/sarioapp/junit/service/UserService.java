dfec1968b280137d14507d8800e70696b0d5adff
so_happy <sarioven1@gmail.com>
2021-08-09 21:36:16 +0300

    Added lifecycle model

--- src/main/java/com/sarioapp/junit/service/UserService.java
+++ src/main/java/com/sarioapp/junit/service/UserService.java
@@ -2,11 +2,18 @@ package com.sarioapp.junit.service;
 
 import com.sarioapp.junit.dto.User;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 public class UserService {
+    List<User> users = new ArrayList<>();
+
+    public boolean add(User user) {
+        return users.add(user);
+    }
+
     public List<User> getAll() {
-        return Collections.emptyList();
+        return users;
     }
 }
--- src/test/java/com/sarioapp/junit/service/UserServiceTest.java
+++ src/test/java/com/sarioapp/junit/service/UserServiceTest.java
@@ -1,18 +1,54 @@
 package com.sarioapp.junit.service;
 
 import com.sarioapp.junit.dto.User;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 import java.util.List;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+@TestInstance(TestInstance.Lifecycle.PER_METHOD)
 class UserServiceTest {
+
+    private UserService userService;
+
+    @BeforeAll
+    static void init() {
+        System.out.println("Before all: ");
+    }
+
+    @BeforeEach
+    void prepare() {
+        System.out.println("Before each: " + this);
+        userService = new UserService();
+    }
 
     @Test
     void usersIsEmptyIfNoUsersAdded() {
-        UserService userService = new UserService();
+        System.out.println("usersIsEmptyIfNoUsersAdded: " + this);
         List<User> users = userService.getAll();
         assertTrue(users.isEmpty());
+    }
+
+    @Test
+    void usersSizeIfUserAdded() {
+        System.out.println("usersSizeIfUserAdded: " + this);
+        userService.add(new User());
+        userService.add(new User());
+
+        List<User> users = userService.getAll();
+
+        assertEquals(2, users.size());
+    }
+
+    @AfterEach
+    void deleteDataFromDatabase() {
+        System.out.println("After each: " + this);
+    }
+
+    static @AfterAll
+    void closeConnectionPool() {
+        System.out.println("After all: ");
     }
 }
