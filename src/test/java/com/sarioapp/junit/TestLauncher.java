25b82b8404a2ba83e505257848bed66758d50bcc
so_happy <sarioven1@gmail.com>
2021-08-09 22:05:08 +0300

    Added launcher class

---
+++ src/test/java/com/sarioapp/junit/TestLauncher.java
@@ -0,0 +1,29 @@
+package com.sarioapp.junit;
+
+import org.junit.platform.engine.discovery.DiscoverySelectors;
+import org.junit.platform.launcher.Launcher;
+import org.junit.platform.launcher.LauncherDiscoveryRequest;
+import org.junit.platform.launcher.core.LauncherDiscoveryRequestBuilder;
+import org.junit.platform.launcher.core.LauncherFactory;
+import org.junit.platform.launcher.listeners.SummaryGeneratingListener;
+
+import java.io.PrintWriter;
+
+public class TestLauncher {
+    public static void main(String[] args) {
+        Launcher launcher = LauncherFactory.create();
+
+        SummaryGeneratingListener listener = new SummaryGeneratingListener();
+
+        LauncherDiscoveryRequest request = LauncherDiscoveryRequestBuilder
+                .request()
+                .selectors(DiscoverySelectors.selectPackage("com.sarioapp.junit.service"))
+                .build();
+
+        launcher.execute(request, listener);
+
+        try (PrintWriter printWriter = new PrintWriter(System.out)) {
+            listener.getSummary().printTo(printWriter);
+        }
+    }
+}
